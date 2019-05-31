912f23fc5a4ece35dade8ee31aad8c23cc0f0ca5
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-31 18:50:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
-class Artist
-  attr_accessor :name
+cclass Artist
+  attr_accessor :name, :genre
  
-  def initialize(name)
+  def initialize(name, genre) 
     @name = name
-
+    @genre = genre
   end
  
 end
